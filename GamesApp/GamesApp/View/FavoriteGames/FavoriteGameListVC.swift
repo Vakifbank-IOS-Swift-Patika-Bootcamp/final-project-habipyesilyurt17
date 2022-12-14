164477ef0a1cf1eb89729b1abe894d0d04d98195
Habip Yesilyurt <yesilyurthabip@hotmail.com>
2022-12-14 16:56:20 +0300

    Create UI

---
+++ GamesApp/GamesApp/View/FavoriteGames/FavoriteGameListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteGameListVC.swift
+//  GamesApp
+//
+//  Created by Habip Yeşilyurt on 14.12.2022.
+//
+
+import UIKit
+
+final class FavoriteGameListVC: BaseVC {
+    @IBOutlet weak var favoriteGamesTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
